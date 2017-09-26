8b8dd7f38515514516d2a2e9dc76e138757457d4
aidonjack <aidonjack@gmail.com>
2017-09-26 00:48:12 -0300

    Redirecionamento Menu para tela inicial jogo das pas e jogo das palavras

--- app/src/main/java/com/dev2/sa/sistemaacessibilidade/TelaActivity.java
+++ app/src/main/java/com/dev2/sa/sistemaacessibilidade/TelaActivity.java
@@ -1,7 +1,9 @@
 package com.dev2.sa.sistemaacessibilidade;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TelaActivity extends AppCompatActivity {
 
@@ -10,4 +12,16 @@ public class TelaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela);
     }
+
+    public void eventosFases(View view){
+
+        if (view.getId() == R.id.fase_1)
+            startActivity(new Intent(this, JogoDasPasActivity.class));
+        else if (view.getId() == R.id.fase_2)
+            startActivity(new Intent(this, JogoDeOrdernarActivity.class));
+    }
+
+
+
+
 }
