0908cd29a7edbc1bfff1c501d67e40925dc218a7
GUILHERME <gsdsilva@restinga.ifrs.edu.br>
2017-09-26 22:15:25 -0300

    validações jogo das frutas com mapeamento

--- app/src/main/java/com/dev2/sa/sistemaacessibilidade/JogoDasFrutasActivity.java
+++ app/src/main/java/com/dev2/sa/sistemaacessibilidade/JogoDasFrutasActivity.java
@@ -14,9 +14,12 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import java.util.HashMap;
+
 public class JogoDasFrutasActivity extends AppCompatActivity {
     View pa_amarela_view, balde_view;
     LinearLayout pa_layout, balde_layout;
+    HashMap<String, String> Balde_Pa = new HashMap<>(14,1);
     int pontos = 0;
 
     @Override
@@ -37,6 +40,14 @@ public class JogoDasFrutasActivity extends AppCompatActivity {
         findViewById(R.id.centerrigth).setOnDragListener(new MyOnDragListener(4));
         findViewById(R.id.bottomleft).setOnDragListener(new MyOnDragListener(5));
         findViewById(R.id.bottomright).setOnDragListener(new MyOnDragListener(6));
+
+
+
+
+        Balde_Pa.put(String.valueOf(R.id.uva_colorida), String.valueOf(R.id.bottomright));
+        Balde_Pa.put(String.valueOf(R.id.abacaxi_colorido), String.valueOf(R.id.centerrigth));
+        Balde_Pa.put(String.valueOf(R.id.laranja_colorida), String.valueOf(R.id.topright));
+
     }
 
     class MyOnLongClickListener implements OnLongClickListener {
@@ -64,26 +75,30 @@ public class JogoDasFrutasActivity extends AppCompatActivity {
         @Override
         public boolean onDrag(View v, DragEvent event) {
             int action = event.getAction();
+            for(String frutaselecionada: Balde_Pa.keySet()) {
+                String sombra = Balde_Pa.get(frutaselecionada);
+                String test = "";
 
-            switch(action){
-                case DragEvent.ACTION_DRAG_STARTED:
-                    // Log para entender o funcionamento (Android Monitor)
-                    Log.i("Script", num+" - ACTION_DRAG_STARTED");
-                    if(event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)){
-                        return(true);
-                    }
-                    return(false);
-                case DragEvent.ACTION_DRAG_ENTERED:
-                    Log.i("Script", num+" - ACTION_DRAG_ENTERED");
-                    //v.setBackgroundColor(Color.YELLOW);
-                    break;
-                case DragEvent.ACTION_DRAG_LOCATION:
-                    Log.i("Script", num+" - ACTION_DRAG_LOCATION");
-                    break;
-                case DragEvent.ACTION_DRAG_EXITED:
-                    Log.i("Script", num+" - ACTION_DRAG_EXITED");
-                    //v.setBackgroundColor(Color.BLUE);
-                    break;
+
+                switch (action) {
+                    case DragEvent.ACTION_DRAG_STARTED:
+                        // Log para entender o funcionamento (Android Monitor)
+                        Log.i("Script", num + " - ACTION_DRAG_STARTED");
+                        if (event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
+                            return (true);
+                        }
+                        return (false);
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        Log.i("Script", num + " - ACTION_DRAG_ENTERED");
+                        //v.setBackgroundColor(Color.YELLOW);
+                        break;
+                    case DragEvent.ACTION_DRAG_LOCATION:
+                        Log.i("Script", num + " - ACTION_DRAG_LOCATION");
+                        break;
+                    case DragEvent.ACTION_DRAG_EXITED:
+                        Log.i("Script", num + " - ACTION_DRAG_EXITED");
+                        //v.setBackgroundColor(Color.BLUE);
+                        break;
                 /*
                 case DragEvent.ACTION_DROP:
                     Log.i("Script", num+" - ACTION_DROP");
@@ -97,51 +112,68 @@ public class JogoDasFrutasActivity extends AppCompatActivity {
                     break;
                 */
 
-                case DragEvent.ACTION_DROP:
-                    Log.i("Script", num+" - ACTION_DROP");
-                    // Move a imagem de um container para outro (6 linhas abaixo)
-                    View view = (View) event.getLocalState();//aqui entra quem está sendo movido
-                    if(view.getId() == R.id.abacaxi_colorido && v.getId() == R.id.centerrigth) {
-                        Toast.makeText(JogoDasFrutasActivity.this, "ACERTOU!!", Toast.LENGTH_SHORT).show();
-                        ViewGroup owner = (ViewGroup) view.getParent();
-                        owner.removeView(view);
-                        LinearLayout container = (LinearLayout) v;
-                        container.addView(view);
-                        pontos++;
-                        view.setVisibility(View.VISIBLE);
-                        view.setEnabled(false);
-                        v.setEnabled(false);
-                    } else if(view.getId() == R.id.uva_colorida && v.getId() == R.id.bottomright){
-                        Toast.makeText(JogoDasFrutasActivity.this, "ACERTOU!!!", Toast.LENGTH_SHORT).show();
-                        ViewGroup owner = (ViewGroup) view.getParent();
-                        owner.removeView(view);
-                        LinearLayout container = (LinearLayout) v;
-                        container.addView(view);
-                        pontos++;
-                        view.setVisibility(View.VISIBLE);
-                        view.setEnabled(false);
-                        v.setEnabled(false);
-                    } else if(view.getId() == R.id.laranja_colorida && v.getId() == R.id.topright){
-                        Toast.makeText(JogoDasFrutasActivity.this, "ACERTOU!!!", Toast.LENGTH_SHORT).show();
-                        ViewGroup owner = (ViewGroup) view.getParent();
-                        owner.removeView(view);
-                        LinearLayout container = (LinearLayout) v;
-                        container.addView(view);
-                        pontos++;
-                        view.setVisibility(View.VISIBLE);
-                        view.setEnabled(false);
-                        v.setEnabled(false);
-                    }if(pontos == 3){
-                    Toast.makeText(JogoDasFrutasActivity.this, "PARABÉNS, VOCÊ GANHOU!!!", Toast.LENGTH_SHORT).show();
-                    //AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                    //builder.setMessage("TESTE").setPositiveButton("husahduihsaud", new  DialogInterface.OnclickListener());
-                }
-                    break;
+                    case DragEvent.ACTION_DROP:
+                        Log.i("Script", num + " - ACTION_DROP");
+                        // Move a imagem de um container para outro (6 linhas abaixo)
+                        View view = (View) event.getLocalState();//aqui entra quem está sendo movido
+                        if(view.getId() == Integer.parseInt(frutaselecionada) && v.getId() == Integer.parseInt(sombra))
+                        {
+                            Toast.makeText(JogoDasFrutasActivity.this, "ACERTOU!!", Toast.LENGTH_SHORT).show();
+                            ViewGroup owner = (ViewGroup) view.getParent();
+                            owner.removeView(view);
+                            LinearLayout container = (LinearLayout) v;
+                            container.addView(view);
+                            pontos++;
+                            view.setVisibility(View.VISIBLE);
+                            view.setEnabled(false);
+                            v.setEnabled(false);
+                        }
+                        if (pontos == 3) {
+                            Toast.makeText(JogoDasFrutasActivity.this, "PARABÉNS, VOCÊ GANHOU!!!", Toast.LENGTH_SHORT).show();
+                            //AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                            //builder.setMessage("TESTE").setPositiveButton("husahduihsaud", new  DialogInterface.OnclickListener());
+                        }
+                        break;
+                       /* if (view.getId() == R.id.abacaxi_colorido && v.getId() == R.id.centerrigth) { /////descomentar essa linhas depois
+                            Toast.makeText(JogoDasFrutasActivity.this, "ACERTOU!!", Toast.LENGTH_SHORT).show();
+                            ViewGroup owner = (ViewGroup) view.getParent();
+                            owner.removeView(view);
+                            LinearLayout container = (LinearLayout) v;
+                            container.addView(view);
+                            pontos++;
+                            view.setVisibility(View.VISIBLE);
+                            view.setEnabled(false);
+                            v.setEnabled(false);
 
-                case DragEvent.ACTION_DRAG_ENDED:
-                    Log.i("Script", num+" - ACTION_DRAG_ENDED");
-                    //v.setBackgroundColor(Color.BLUE);
-                    break;
+
+                        } else if (view.getId() == R.id.uva_colorida && v.getId() == R.id.bottomright) {
+                            Toast.makeText(JogoDasFrutasActivity.this, "ACERTOU!!!", Toast.LENGTH_SHORT).show();
+                            ViewGroup owner = (ViewGroup) view.getParent();
+                            owner.removeView(view);
+                            LinearLayout container = (LinearLayout) v;
+                            container.addView(view);
+                            pontos++;
+                            view.setVisibility(View.VISIBLE);
+                            view.setEnabled(false);
+                            v.setEnabled(false);
+                        } else if (view.getId() == R.id.laranja_colorida && v.getId() == R.id.topright) {
+                            Toast.makeText(JogoDasFrutasActivity.this, "ACERTOU!!!", Toast.LENGTH_SHORT).show();
+                            ViewGroup owner = (ViewGroup) view.getParent();
+                            owner.removeView(view);
+                            LinearLayout container = (LinearLayout) v;
+                            container.addView(view);
+                            pontos++;
+                            view.setVisibility(View.VISIBLE);
+                            view.setEnabled(false);
+                            v.setEnabled(false);
+                        }
+
+
+                    case DragEvent.ACTION_DRAG_ENDED:
+                        Log.i("Script", num + " - ACTION_DRAG_ENDED");
+                        //v.setBackgroundColor(Color.BLUE);
+                        break;*/
+                }
             }
             return true;
         }
