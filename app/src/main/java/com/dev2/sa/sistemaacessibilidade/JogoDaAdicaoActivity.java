eb8586ce54bc17cefacb6924b6a60d20589a4d12
aidonjack <aidonjack@gmail.com>
2017-10-21 20:03:19 -0200

    criação das imagens e da activity do jogo de somar

---
+++ app/src/main/java/com/dev2/sa/sistemaacessibilidade/JogoDaAdicaoActivity.java
@@ -0,0 +1,13 @@
+package com.dev2.sa.sistemaacessibilidade;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JogoDaAdicaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogo_da_adicao);
+    }
+}
