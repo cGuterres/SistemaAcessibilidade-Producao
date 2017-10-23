56967de7a43fa74e7dcabfb8c106cb51d2b37668
LevyMarcelo <lmgcunha@restinga.ifrs.edu.br>
2017-10-23 20:53:27 -0200

    Segunda tela de menu

--- app/src/main/java/com/dev2/sa/sistemaacessibilidade/TelaActivity2.java
+++ app/src/main/java/com/dev2/sa/sistemaacessibilidade/TelaActivity2.java
@@ -2,39 +2,30 @@ package com.dev2.sa.sistemaacessibilidade;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class TelaActivity extends AppCompatActivity {
+public class TelaActivity2 extends AppCompatActivity {
 
     MediaPlayer mp = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela);
+        setContentView(R.layout.activity_tela2);
 
         mp.stop();
         playMusic();
     }
 
-    public void eventosFases(View view) {
-
-        if (view.getId() == R.id.jogo_das_pas)
-            startActivity(new Intent(this, JogoDasPasActivity.class));
-        else if (view.getId() == R.id.jogo_das_frutas)
-            startActivity(new Intent(this, JogoDasFrutasActivity.class));
-        else if (view.getId() == R.id.jogo_de_ordenar)
-            startActivity(new Intent(this, Jogo_de_Ordenar_Palavra2.class));
-        else if (view.getId() == R.id.jogo_de_ordenar_palavra)
-            startActivity(new Intent(this, JogoDeOrdernarActivity.class));
-        else if (view.getId() == R.id.jogo_dos_vizinhos)
+    public void returnMenu(View view) {
+        if (view.getId() == R.id.jogo_dos_vizinhos)
             startActivity(new Intent(this, JogoDosVizinhosActivity.class));
         else if (view.getId() == R.id.jogodaAdicao)
             startActivity(new Intent(this, JogoDaAdicaoActivity.class));
-        else if(view.getId() == R.id.outra_fase)
-            startActivity(new Intent(this, TelaActivity2.class));
+        else if(view.getId() == R.id.retorna_fase)
+            startActivity(new Intent(this, TelaActivity.class));
     }
 
     @Override
