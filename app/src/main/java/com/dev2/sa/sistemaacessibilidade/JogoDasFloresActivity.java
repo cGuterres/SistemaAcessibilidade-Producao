d38bd3ade4609ca20650f9deb19952c1c65406cb
LevyMarcelo <lmgcunha@restinga.ifrs.edu.br>
2017-11-12 21:34:11 -0200

    Documentação do jogo das flores

--- app/src/main/java/com/dev2/sa/sistemaacessibilidade/JogoDasFloresActivity.java
+++ app/src/main/java/com/dev2/sa/sistemaacessibilidade/JogoDasFloresActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.View.DragShadowBuilder;
 import android.view.View.OnDragListener;
 import android.view.View.OnLongClickListener;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -20,7 +19,7 @@ public class JogoDasFloresActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jogo_das_flores);
 
-        // Pego o elemento flor e seto como um clicavel e declaro como um objeto
+        // Pego o elemento flor_azul e seto como um elemento clicavel
         findViewById(R.id.flor_azul).setOnLongClickListener(new MyOnLongClickListener());
         findViewById(R.id.flor_laranja).setOnLongClickListener(new MyOnLongClickListener());
         findViewById(R.id.flor_verde).setOnLongClickListener(new MyOnLongClickListener());
@@ -28,7 +27,7 @@ public class JogoDasFloresActivity extends Activity {
         findViewById(R.id.flor_laranja2);
         findViewById(R.id.flor_verde2);
 
-        // Pego o layout e seto como um drag e declaro como um objeto
+        // Pego o layout topleft e seto como um drag
         findViewById(R.id.topleft).setOnDragListener(new MyOnDragListener(1)); // flor azul
         findViewById(R.id.topright).setOnDragListener(new MyOnDragListener(2));// flor verde 2
         findViewById(R.id.centerleft).setOnDragListener(new MyOnDragListener(3)); // flor laranja
@@ -37,11 +36,13 @@ public class JogoDasFloresActivity extends Activity {
         findViewById(R.id.bottomright).setOnDragListener(new MyOnDragListener(6));// flor azul 2
     }
 
+    // classe que implementa o click longo da imagem
     class MyOnLongClickListener implements OnLongClickListener {
         @Override
         public boolean onLongClick(View v) {
+            // Permite que o OnDrag receba ou não o elemento que está em drag
             ClipData data = ClipData.newPlainText("simple_text", "text");
-            //DragShadowBuilder sb = new View.DragShadowBuilder(findViewById(R.id.shadow));
+            // sombra da imagem
             DragShadowBuilder sb = new DragShadowBuilder(v);
             // Esconde a imagem quando for arrastar a sua sombra
             v.setVisibility(View.INVISIBLE);
@@ -50,6 +51,7 @@ public class JogoDasFloresActivity extends Activity {
         }
     }
 
+    // Classe que implementa o metodo para arrastar a imagem
     class MyOnDragListener implements OnDragListener {
         private int num;
 
@@ -59,15 +61,39 @@ public class JogoDasFloresActivity extends Activity {
             this.num = num;
         }
 
+        // v = MyOnDragListener
+        // event = startDrag
         @Override
         public boolean onDrag(View v, DragEvent event) {
             int action = event.getAction();
             boolean hit = false;
-            View view = (View) event.getLocalState();//aqui entra quem está sendo
+            // Cria a imagem que acessa a v (imagem principal) do startDrag
+            View view = (View) event.getLocalState();
                  switch (action) {
+                    // Quando tem ação de drop deixa a sombra sempre visível
+                    case DragEvent.ACTION_DRAG_ENDED:
+                        view.setVisibility(View.VISIBLE);
+                        break;
+                    // Realiza a movimentação das imagens de um layout para o outro
                     case DragEvent.ACTION_DROP:
                         if(view.getId() == R.id.flor_laranja && v.getId() == R.id.bottomright){
-                            // comentario
+                            // O layout atual recebe a imagem
+                            LinearLayout oldparent = (LinearLayout) view.getParent();
+                            // Remove a imagem do layout atual
+                            oldparent.removeView(view);
+                            // Cria um layout novo
+                            LinearLayout newParent = (LinearLayout)v;
+                            // Adiciona a imagem no novo layout
+                            newParent.addView(view);
+                            // Deixa a imagem visivel
+                            view.setVisibility(View.VISIBLE);
+                            // Bloqueia a imagem
+                            view.setEnabled(false);
+                            // Acerto é verdadeiro
+                            hit = true;
+                            // Soma 1 ponto
+                            pontos++;
+                        } else if(view.getId() == R.id.flor_verde && v.getId() == R.id.topright){
                             LinearLayout oldparent = (LinearLayout) view.getParent();
                             oldparent.removeView(view);
                             LinearLayout newParent = (LinearLayout)v;
@@ -76,18 +102,7 @@ public class JogoDasFloresActivity extends Activity {
                             view.setEnabled(false);
                             hit = true;
                             pontos++;
-                        }
-                        else if(view.getId() == R.id.flor_verde && v.getId() == R.id.topright){
-                            LinearLayout oldparent = (LinearLayout) view.getParent();
-                            oldparent.removeView(view);
-                            LinearLayout newParent = (LinearLayout)v;
-                            newParent.addView(view);
-                            view.setVisibility(View.VISIBLE);
-                            view.setEnabled(false);
-                            hit = true;
-                            pontos++;
-                        }
-                        else if(view.getId() == R.id.flor_azul && v.getId() == R.id.centerrigth){
+                        } else if(view.getId() == R.id.flor_azul && v.getId() == R.id.centerrigth){
                             ////////////////////////////// SOM
                             //Metodos.sound(R.drawable.flor_azul, JogoDasFloresActivity.this);
                             LinearLayout oldparent = (LinearLayout) view.getParent();
@@ -98,16 +113,18 @@ public class JogoDasFloresActivity extends Activity {
                             view.setEnabled(false);
                             hit = true;
                             pontos++;
-                        }
-                        else{
+                        } else{
+                            // Senão acertar exibe a mensagem de erro
                             Toast.makeText(JogoDasFloresActivity.this, "VOCÊ ERROU!", Toast.LENGTH_SHORT).show();
+                            // Deixa a imagem visivel
                             view.setVisibility(View.VISIBLE);
                         }
                         if(pontos <= 2 && hit) {
+                            // Se o número de acertos for menor ou igual a 2, exibe a mensagem de acerto
                             Metodos.ShowHitMessage(JogoDasFloresActivity.this, "VOCÊ ACERTOU!");
-
                         }
                         if(pontos == 3) {
+                            // Se o número de acertos for igual a 3, exibe o dialog de vitoria e as opções de voltar para o menu ou reiniciar o jogo
                             Metodos.ShowDialog(JogoDasFloresActivity.this, R.drawable.flor_verde, "PARABÉNS", "VOCÊ GANHOU!");
                         }
                         break;
